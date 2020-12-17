81eea80d835c4074ee7bdd6c9190dc60621d7396
Martin Ober <martinober@MacBook-Air-de-Martin.local>
2020-12-17 20:04:30 -0300

    Chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -2,35 +2,35 @@ var socket = io();
 
 var params = new URLSearchParams(window.location.search);
 
-if(!params.has('nombre') || !params.has('sala')) {
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y la sala son necesarios');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function(resp){
-        console.log('Usuarios conectados', resp);
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('entrarChat: ', resp);
+        renderizarUsuarios(resp);
     });
 
 });
 
 // escuchar
 socket.on('disconnect', function() {
-
     console.log('Perdimos conexión con el servidor');
-
 });
 
+
 // Enviar información
-// socket.emit('enviarMensaje', {
-//     usuario: 'Fernando',
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -38,19 +38,19 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    console.log('crearMensaje:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Escuchar cambios de usuarios, cuando entra o sale del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersonas', function(personas) {
-
-    console.log(personas);
-
+    console.log('listaPersonas: ', personas);
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje privado: ', mensaje);
+    console.log('mensajePrivado: ', mensaje);
 });
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -8,7 +8,7 @@ io.on('connection', (client) => {
 
     client.on('entrarChat', (data, callback) => {
 
-        console.log(data);
+        console.log('entrarChat: ', data);
 
         if(!data.nombre || !data.sala) {
             return callback({
@@ -21,23 +21,33 @@ io.on('connection', (client) => {
 
         usuarios.agregarPersona(client.id, data.nombre, data.sala);
 
+        let mensaje = crearMensaje('Administrador', `${data.nombre} ingreso el chat`);
+        
+        client.broadcast.to(data.sala).emit('crearMensaje', mensaje);
         client.broadcast.to(data.sala).emit('listaPersonas', usuarios.getPersonasPorSala(data.sala));
         
         callback(usuarios.getPersonasPorSala(data.sala));
     });
 
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
         
+        console.log('crearMensaje: ', data);
+
         let persona = usuarios.getPersona(client.id);
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
 
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+        client.broadcast.to(persona.sala).emit('listaPersonas', usuarios.getPersonasPorSala(persona.sala));
+
+        callback(mensaje);
 
     });
 
     client.on('disconnect', () => {
         
         let personaBorrada = usuarios.borrarPesona(client.id);
+        console.log('disconnect: ', personaBorrada);
+        
         let mensaje = crearMensaje('Administrador', `${personaBorrada.nombre} abandono el chat`);
         
         client.broadcast.to(personaBorrada.sala).emit('crearMensaje', mensaje);
@@ -47,6 +57,8 @@ io.on('connection', (client) => {
 
     // Mensajes privados
     client.on('mensajePrivado', (data) => {
+
+        console.log('mensajePrivado: ', data);
 
         let persona = usuarios.getPersona(client.id);
 
